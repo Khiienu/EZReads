cdf29c720fb1b3ca404c66e18262eb26b700eeb0
Conner U <cunderhillosx@gmail.com>
2021-06-28 12:56:58 -0700

    fixed gameshelf

--- db/migrations/20210628194855-create-game-shelve.js
+++ db/migrations/20210628194855-create-game-shelve.js
@@ -1,22 +1,26 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("GamesBySystems", {
+    return queryInterface.createTable("GameShelves", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+      usersId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users" },
+      },
       gameId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: "Games" },
       },
-      systemId: {
+      status: {
         allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: "Systems" },
+        type: Sequelize.STRING(75),
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +33,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("gamesBySystems");
+    return queryInterface.dropTable("GameShelves");
   },
 };
