2e66a6afb26f7acf90cca2f2542ce9e6e566b328
Conner U <cunderhillosx@gmail.com>
2021-06-28 12:59:23 -0700

    gameshelf hasmany games

--- db/models/gameshelve.js
+++ db/models/gameshelve.js
@@ -1,12 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const GameShelve = sequelize.define('GameShelve', {
-    usersId: DataTypes.INTEGER,
-    gameId: DataTypes.INTEGER,
-    status: DataTypes.STRING
-  }, {});
-  GameShelve.associate = function(models) {
-    // associations can be defined here
+  const GameShelve = sequelize.define(
+    "GameShelve",
+    {
+      usersId: DataTypes.INTEGER,
+      gameId: DataTypes.INTEGER,
+      status: DataTypes.STRING,
+    },
+    {}
+  );
+  GameShelve.associate = function (models) {
+    GameShelve.hasMany(models.Game, { foreignKey: "gameId" });
   };
   return GameShelve;
 };
