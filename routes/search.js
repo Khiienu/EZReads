808022fad3bbee0b89366aab14c250a8c15e8d9f
Khiienu <anthonyhuynh123@gmail.com>
2021-06-30 18:26:58 -0500

    implemented randomization in search results

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { asyncHandler } = require("./utils");
 const db = require("../db/models");
-const {Game, System, Genre} = db;
+const {Game, System, Genre, sequelize} = db;
 
 
 router.post(
@@ -14,11 +14,11 @@ router.post(
         if(genre || system){ //confirm whether ANY search properties have been set
             
             if (genre && (!system))
-                games = await Game.findAll({where: {primaryGenre: genre}});
+                games = await Game.findAll({where: {primaryGenre: genre}, order: sequelize.random()});
             else if (system && (!genre)) 
-                games = await Game.findAll({include: [{model: System, where: { system: system }}]});
+                games = await Game.findAll({include: [{model: System, where: { system: system }}], order: sequelize.random()});
             else{
-                games = await Game.findAll({where: {primaryGenre: genre}, include: [{model: System, where: { system: system }}]});
+                games = await Game.findAll({where: {primaryGenre: genre}, include: [{model: System, where: { system: system }}], order: sequelize.random()});
             }
             
             
