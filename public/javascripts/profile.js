7d3718a9b2b1e1caf0f33d3c14680072e5a5c20f
david <sneakblue@gmail.com>
2021-07-01 10:42:20 -0600

    working on home

---
+++ public/javascripts/profile.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let currBtn = document.getElementById('current');
+    let playedBtn = document.getElementById('played');
+    let wantBtn = document.getElementById('want');
+
+    currBtn.addEventListener('click', (e) => {
+        location.assign('')
+    })
+})
